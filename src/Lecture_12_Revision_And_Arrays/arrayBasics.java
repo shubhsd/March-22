d2094a2b9055d87f4f914dbed12a1af5a9f5c57c
shubham <shubhsd@users.noreply.github.com>
2022-04-30 00:49:45 +0530

    decimal to binary and array basics

---
+++ src/Lecture_12_Revision_And_Arrays/arrayBasics.java
@@ -0,0 +1,9 @@
+package Lecture_12_Revision_And_Arrays;
+
+public class arrayBasics {
+    public static void main(String[] args) {
+         int [] shopping_cart = {10,20,30,60,75};
+
+        System.out.println(shopping_cart.length);
+    }
+}
---
+++ src/Lecture_12_Revision_And_Arrays/binaryToDecimalAndViceVersa.java
@@ -0,0 +1,33 @@
+package Lecture_12_Revision_And_Arrays;
+
+public class binaryToDecimalAndViceVersa {
+
+    public static int binary2Decimal(int n) {
+        int ans = 0;
+        int power = 1;
+        while (n != 0) {
+            int last_digit = n % 10;
+            ans = ans + last_digit * power;
+            power = power * 2;
+            n = n / 10;
+        }
+        return ans;
+    }
+
+    public static void decimal2Binary(int n) {
+        int power = 1;
+        int ans = 0;
+        while (n != 0) {
+            int digit = n % 2;
+            ans = ans + power * digit;
+            power = power * 10;
+            n = n / 2;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(binary2Decimal(10101));
+        decimal2Binary(13);
+    }
+}
