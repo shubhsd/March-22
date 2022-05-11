7879485a926e2410a53e66840dcdaffd0788287c
Shubham <shubhamsd53@gmail.com>
2022-05-11 20:16:47 +0530

    test commit

--- src/Lecture_9_Pattern_2/pattern1.java
+++ src/Lecture_9_Pattern_2/pattern1.java
@@ -9,9 +9,10 @@ package Lecture_9_Pattern_2;
 public class pattern1 {
     public static void main(String[] args) {
         int n = 5;
+        
 //    Print n rows
         for (int i = 0; i < n; i++) {
-            
+
 //            Print n-i spaces
             for(int spaces = 0; spaces <= n - i; spaces++ ) {
                 System.out.print(" \t");
