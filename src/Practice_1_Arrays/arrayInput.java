281780cd4fc87765b68e6453747d32bc7cd6249b
Shubham <shubhamsd53@gmail.com>
2022-05-27 08:49:01 +0530

    Taking array input from user and printing new array

--- src/Practice_1_Arrays/arrayInput.java
+++ src/Practice_1_Arrays/arrayInput.java
@@ -6,14 +6,27 @@ public class arrayInput {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        // Allocating array 
+        // Allocating array
 
         int[] arr = new int[n];
 
         // Print array - By default 0 will be printed
 
-        for(int i = 0; i<arr.length; i++) {
-            System.out.println(arr[i]);
+        System.out.println("Default case array ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+        // Printing array after taking data input from user
+        System.out.println("Enter elements of array :  ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        System.out.println("Entered elements of array are :  ");
+        for (int i = 0; i < arr.length; i++) {
+           System.out.println(arr[i]);
         }
     }
 }
