dc5d3c5d85507bb0901d2d32db6ffa0d08b4d6c1
Shubham <shubhamsd53@gmail.com>
2022-05-30 07:42:04 +0530

    gcd of two numbers

---
+++ src/Practice_1_Basics/findGCD.java
@@ -0,0 +1,21 @@
+package Practice_1_Basics;
+
+// GCD - largest number that divides both numbers
+// Approach - loop till smallest number - Brute force
+// We will find common divisor and update value of gcd
+
+public class findGCD {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 20;
+
+        int gcd = 1;
+
+        for(int i = 1; i<=Math.min(a, b); i++) {
+            if(a%i == 0 && b%i==0) {
+                gcd = i;
+            }
+        }
+        System.out.println(gcd);
+    }
+}
