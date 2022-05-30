7f489052c170a74e10b0461a2efe1009840b82b4
Shubham <shubhamsd53@gmail.com>
2022-05-30 07:33:20 +0530

    loop syntax alternative for factorial code

--- src/Practice_1_Basics/factorial.java
+++ src/Practice_1_Basics/factorial.java
@@ -3,10 +3,11 @@ package Practice_1_Basics;
 public class factorial {
     public static void main(String[] args) {
         int ans = 1;
-        int n = 5;
+        int n = 6;
 
         for(int i = 1; i<=n; i++) {
-            ans = ans*i;
+            // ans = ans*i;
+            ans *=i;
         }
 
         System.out.println(ans);
