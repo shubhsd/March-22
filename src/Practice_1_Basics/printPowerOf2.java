85b676cca02487ec245faf5ef36471b7412754f6
Shubham <shubhamsd53@gmail.com>
2022-05-30 07:07:05 +0530

    Power of 2 using while loop

---
+++ src/Practice_1_Basics/printPowerOf2.java
@@ -0,0 +1,11 @@
+package Practice_1_Basics;
+
+public class printPowerOf2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=100) {
+            System.out.println(i);
+            i*=2;
+        }
+    }
+}
