0934cd3c51b55d50f9f3c2e8b80ef01c03f3f965
Shubham <shubhamsd53@gmail.com>
2022-05-30 07:35:26 +0530

    Sum of n terms

--- src/Practice_1_Basics/factorial.java
+++ src/Practice_1_Basics/factorial.java
@@ -3,13 +3,20 @@ package Practice_1_Basics;
 public class factorial {
     public static void main(String[] args) {
         int ans = 1;
-        int n = 6;
+        int n = 4;
 
         for(int i = 1; i<=n; i++) {
             // ans = ans*i;
             ans *=i;
         }
 
-        System.out.println(ans);
+        System.out.println("Factorial " + ans);
+        ans = 0;
+        for(int i = 1; i<=n; i++) {
+            // ans = ans*i;
+            ans +=i;
+        }
+
+        System.out.println("Sum of n terms " + ans);
     }
 }
