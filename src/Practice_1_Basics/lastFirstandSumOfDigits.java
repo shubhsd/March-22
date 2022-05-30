265bd177ff37947ef42e2f059dd81cea65597b5f
Shubham <shubhamsd53@gmail.com>
2022-05-30 08:07:59 +0530

    Last digit of a number

---
+++ src/Practice_1_Basics/lastFirstandSumOfDigits.java
@@ -0,0 +1,15 @@
+package Practice_1_Basics;
+
+
+// Given a number n find 
+// 1. First digit of a number
+// 2. Last digit of a number
+// 3. Sum of digits
+
+
+public class lastFirstandSumOfDigits {
+    public static void main(String[] args) {
+        int n = 2356;
+        System.out.println("Last digit : " + n%10);
+    }
+}
