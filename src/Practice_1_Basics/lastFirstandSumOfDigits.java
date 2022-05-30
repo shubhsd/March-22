ac308a70c4732f6c837ebb7db3809bf83b03883c
Shubham <shubhamsd53@gmail.com>
2022-05-30 08:10:04 +0530

    first digit and sum of a number

--- src/Practice_1_Basics/lastFirstandSumOfDigits.java
+++ src/Practice_1_Basics/lastFirstandSumOfDigits.java
@@ -1,15 +1,24 @@
 package Practice_1_Basics;
 
-
 // Given a number n find 
 // 1. First digit of a number
 // 2. Last digit of a number
 // 3. Sum of digits
 
-
 public class lastFirstandSumOfDigits {
     public static void main(String[] args) {
         int n = 2356;
-        System.out.println("Last digit : " + n%10);
+        System.out.println("Last digit : " + n % 10);
+        int sum = 0;
+
+        int digit = 0;
+
+        while (n > 0) {
+            digit = n % 10;
+            sum = sum + digit;
+            n = n / 10;
+        }
+        System.out.println("Sum is : " + sum);
+        System.out.println("First digit is : " + digit);
     }
 }
