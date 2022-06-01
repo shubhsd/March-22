b07257d19373243fdf3df089cf6fb4afdcf99410
Shubham <shubhamsd53@gmail.com>
2022-06-01 07:12:29 +0530

    Method 2 to find gcd

--- src/Practice_1_Basics/findGCD.java
+++ src/Practice_1_Basics/findGCD.java
@@ -9,13 +9,27 @@ public class findGCD {
         int a = 12;
         int b = 20;
 
-        int gcd = 1;
+        // int gcd = 1;
 
-        for(int i = 1; i<=Math.min(a, b); i++) {
-            if(a%i == 0 && b%i==0) {
+        // for(int i = 1; i<=Math.min(a, b); i++) {
+        //     if(a%i == 0 && b%i==0) {
+        //         gcd = i;
+        //     }
+        // }
+        // System.out.println(gcd);
+        int min = 1;
+        if(a<b) {
+            min = a;
+        }else {
+            min = b;
+        }
+
+        int gcd = 1;
+        for(int i = 1; i<=min; i++) {
+            if(a%i == 0 && b%i== 0) {
                 gcd = i;
             }
         }
-        System.out.println(gcd);
+        System.out.println("gcd " + gcd);
     }
 }
