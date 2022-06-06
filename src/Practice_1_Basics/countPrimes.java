b8f10ea7f5f6973202da421fac7f2242aa65286a
Shubham <shubhamsd53@gmail.com>
2022-06-06 19:08:17 +0530

    Reverse number without printing digits in reverse

---
+++ src/Practice_1_Basics/countPrimes.java
@@ -0,0 +1,11 @@
+package Practice_1_Basics;
+
+public class countPrimes {
+    public static void main(String[] args) {
+        int limit = 7;
+        for(int cnt = 0, i= 2; cnt<limit; i++ ) {
+            // Increment cnt when you find a prime number   
+                        
+        }
+    }
+}
---
+++ src/Practice_2_Typecasting/reverseNumWithoutPrintingDigits.java
@@ -0,0 +1,19 @@
+package Practice_2_Typecasting;
+
+import java.util.Scanner;
+
+public class reverseNumWithoutPrintingDigits {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int ans = 0;
+
+        while(n>0) {
+            int rem = n % 10;
+            ans = ans * 10  + rem; 
+            n = n/10;
+        }
+        System.out.println(ans);
+    }
+}
