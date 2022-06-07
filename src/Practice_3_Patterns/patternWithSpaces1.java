15b84a5c7c6fdddf0b87a16fc26703fcf064eec9
Shubham <shubhamsd53@gmail.com>
2022-06-07 19:17:28 +0530

    Pattern problem in progress

---
+++ src/Practice_3_Patterns/patternWithSpaces1.java
@@ -0,0 +1,18 @@
+package Practice_3_Patterns;
+
+import java.util.Scanner;
+
+public class patternWithSpaces1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        for(int i = 1; i<n; i++) {
+            // for rows
+
+            // for(int spaces = 1; )
+            System.out.println();
+        }
+    }
+}
