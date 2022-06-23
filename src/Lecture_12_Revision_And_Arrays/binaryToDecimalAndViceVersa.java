5be6d77858e6cb53a9afdc44713890c06bb9a712
Shubham <shubhamsd53@gmail.com>
2022-06-23 19:58:41 +0530

    Test commit

--- src/Lecture_12_Revision_And_Arrays/binaryToDecimalAndViceVersa.java
+++ src/Lecture_12_Revision_And_Arrays/binaryToDecimalAndViceVersa.java
@@ -25,7 +25,6 @@ public class binaryToDecimalAndViceVersa {
         }
         System.out.println(ans);
     }
-    
 
     public static void main(String[] args) {
 //        System.out.println(binary2Decimal(10101));
